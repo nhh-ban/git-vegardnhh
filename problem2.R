dc0a5e0265800d57660f219300d84042ae3f68d2
Vegard Sorheim <vegard.sorheim@student.nhh.no>
2023-10-09 13:24:41 +0200

    problem3

--- problem2.R
+++ problem2.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggplot2)
 
 # Step 1: Read in the file as lines
 lines <- read_lines("suites_dw_Table1.txt")
@@ -29,5 +30,37 @@ galaxies <- read_delim("temp_data.txt", delim = "|", col_names = col_names, trim
 file.remove("temp_data.txt")
 
 head(galaxies)
+
+
+# Problem 3
+
+# Reshape data to long format
+long_data <- galaxies %>%
+  select(a_26, log_lk, log_m26, log_mhi) %>%  # Select relevant columns
+  gather(key = "variable", value = "value")   # Convert to long format
+
+# Remove NA values
+long_data <- long_data %>% filter(!is.na(value))
+
+# Ensure 'value' column is treated as numeric
+long_data$value <- as.numeric(as.character(long_data$value))
+
+# Now you can try to plot again
+ggplot(long_data, aes(x = value)) +
+  geom_histogram(binwidth = 0.5, fill = "skyblue", color = "white", alpha = 0.7) +
+  facet_wrap(~variable, scales = 'free_x') +
+  labs(title = "Histograms of Galaxy Characteristics",
+       x = "Value",
+       y = "Frequency") +
+  theme_minimal()
+
+
+# Possible Expplanation:
+# It might be due to the limitations in the observational techniques 
+# and instruments used to collect the data 
+# (smaller galaxies might be harder to detect, especially if they are farther away). 
+# Alternatively, it might reflect a real characteristic of the volume of space being sampled 
+# (perhaps it contains fewer small galaxies for some reason)
+
 
 
